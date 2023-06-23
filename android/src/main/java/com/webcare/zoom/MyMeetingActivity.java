41171f4664f8579b53a046c46d364be390de3046
Moaz El-sawaf <moazelsawaf@hotmail.com>
2023-06-23 18:36:23 +0200

    feat: :sparkles: embed zoom meeting ui in a separate activity

---
+++ android/src/main/java/com/webcare/zoom/MyMeetingActivity.java
@@ -0,0 +1,13 @@
+package com.webcare.zoom;
+
+import us.zoom.sdk.MeetingActivity;
+import android.os.Bundle;
+import android.view.WindowManager;
+
+public class MyMeetingActivity extends MeetingActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+}
