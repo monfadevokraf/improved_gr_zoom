2e06e68aa596322d339f2b5a4610a3f681467f9b
Moaz El-sawaf <moazelsawaf@hotmail.com>
2023-06-23 18:42:52 +0200

    feat: :sparkles: add flag secure to the meeting ui activity

--- android/src/main/java/com/webcare/zoom/MyMeetingActivity.java
+++ android/src/main/java/com/webcare/zoom/MyMeetingActivity.java
@@ -9,5 +9,9 @@ public class MyMeetingActivity extends MeetingActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+
+		if (!BuildConfig.DEBUG) {
+			getWindow().setFlags(WindowManager.LayoutParams.FLAG_SECURE, WindowManager.LayoutParams.FLAG_SECURE);
+		}
 	}
 }
